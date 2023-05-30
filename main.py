6d94b5d16fe7dc82c693ef3fef28995eb2589c0f
Mykhailo Lazoryk <mikolaz2727@gmail.com>
2023-05-30 22:19:50 +0300

    Added views to flaks

--- main.py
+++ main.py
@@ -1,45 +1,75 @@
-from flask import Flask
+import pprint
+import random
+import string
+
+import requests
+
+from http import HTTPStatus
+
+from flask import Flask, request, Response
+from webargs import fields, validate
+from webargs.flaskparser import use_kwargs
+
 
 app = Flask(__name__)
 
 
-@app.route("/ad min")
-def hello_world():
-    return ['1','2','3','4 ']
-
-# /admin
-# /admin
-
-# ?#&~/:@%+
-
-# https://flask.palletsprojects.com:443
-# https | http
-# flask.palletsprojects.com - Domain
-# facebook
-# 157.240.224.20:8000/
-# 157.240.224.20:
-
-# https://flask.palletsprojects.com/admin/
+@app.route("/<int:counter>/")
+def hello_world(counter):
+    return [str(i) for i in range(int(counter))]
 
 
-# def generate_password():
-#     """
-#     from 10 to 20 chars
-#     upper and lower case
-#     """
-#     # st  ring
-#     # asci i_lowercase
-#     # asci i_uppercase
-#     # int
-#     # special symbols
-#     # return password
-#     pass
+@app.route("/")
+def hello_world_with_args():
+    my_args = request.args.get('a')
+    return my_args
 
-# def calculate_average():
-#     """
-#     c  sv file with students
-#     1. calculate average high
-#     2.calculate average weight
-#     csv - use lib
-#     *pandas - use pandas for calculating
-#     """
+
+# MyFirstName - camel
+# my_first_name - snake
+# my-first-name - kebab
+
+@app.route("/generate-password")
+@use_kwargs(
+    {
+        "password_length": fields.Int(
+            missing=15,
+            validate=[validate.Range(min=8, max=100)]
+        ),
+    },
+    location="query"
+)
+def generate_password(password_length: int):
+    # password_length = 10
+    # request.args.get('length', '15')
+
+    # if not password_length.isdigit():
+    #     return "ERROR: should be a digit"
+    # password_length = int(password_length)
+    #
+    # if not 8 <= password_length <= 100:
+    #     return "ERROR: should be in range [8, 100]"
+
+    return "".join(
+        random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation,
+                       k=password_length))
+
+
+@app.route("/get-astronauts")
+def get_astronauts():
+    url = "http://api.open-notify.org/astros.json"
+    result = requests.get(url, {})
+
+    if result.status_code not in (HTTPStatus.OK,):
+        return Response("ERROR: Something went wrong", status=result.status_code)
+
+    result: dict = result.json()
+    statistics = {}
+    for entry in result.get("people", {}):
+        statistics[entry["craft"]] = statistics.get(entry["craft"], 0) + 1
+
+    return statistics
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
